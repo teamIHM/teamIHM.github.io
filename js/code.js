d3065cf64a97838c9b3f26d5f1f653093b0870ec
KillCo18 <corentin.arn@gmail.com>
2019-03-08 18:09:10 +0100

    aa

--- js/code.js
+++ js/code.js
@@ -7,38 +7,38 @@ function trid(str) {
 }
 
 function addQuestionItem() {
-	let input = document.getElementById("questionText");
-	if(document.getElementById(trid(input.value.trim())) || document.getElementById(trid("text"+input.value.trim())) || document.getElementById(trid("list"+input.value.trim()))) {
+	let input = document.getElementById("questionText").value;
+	if(document.getElementById(trid(input.trim())) || document.getElementById(trid("text"+input.trim())) || document.getElementById(trid("list"+input.trim()))) {
 		return;
 	}
-	if(htmlEntities(input.value.trim())) {
+	if(htmlEntities(input.trim())) {
 		let divQuestion = document.createElement("DIV");
 		divQuestion.className = "question";
-		divQuestion.id = trid(input.value.trim().split(' ').join('_'));
-		divQuestion.innerHTML = "<p>" + htmlEntities(input.value) + "<p>";
-		divQuestion.innerHTML += "<ul id=\"list"+trid(input.value.trim().split(' ').join('_'))+"\"></ul>";
-		divQuestion.innerHTML += "<button class=\"btn\" id=\"addChoice_"+trid(input.value.trim().split(' ').join('_'))+"\" onclick=\"addChoice("+divQuestion.id+")\"/>Ajouter une proposition</button>";
-		divQuestion.innerHTML += "<input type=\"text\" id=\"text"+trid(input.value.trim().split(' ').join('_'))+"\"/>";
-		input.value = "";
+		divQuestion.id = trid(input.replace(/[^a-zA-Z]/g, ""));
+		divQuestion.innerHTML = "<p>" + htmlEntities(input) + "<p>";
+		divQuestion.innerHTML += "<ul id=\"list"+input.replace(/[^a-zA-Z]/g, "")+"\"></ul>";
+		divQuestion.innerHTML += "<button class=\"btn\" id=\"addChoice_"+input.replace(/[^a-zA-Z]/g, "")+"\" onclick=\"addChoice("+divQuestion.id+")\"/>Ajouter une proposition</button>";
+		divQuestion.innerHTML += "<input type=\"text\" id=\"text"+input.replace(/[^a-zA-Z]/g, "")+"\"/>";
+		input = "";
 		document.getElementById("QCMForm").append(divQuestion);
 	}
 }
 
 function addChoice(questionID) {
 	console.log("1 OK");
-	let input = document.getElementById(trid("text"+questionID.id));
+	let input = document.getElementById(trid("text"+questionID.id)).value;
 	console.log("1 OK");
-	if(document.getElementById(trid(input.value.trim().split(' ').join('_')+"_"+questionID.id))) {
+	if(document.getElementById(trid(input.replace(/[^a-zA-Z ]/g, "")+"_"+questionID.id))) {
 		return;
 	}
 	console.log("1 OK");
-	if(input.value) {
+	if(input) {
 		let listElem = document.createElement("LI");
 	console.log("1 OK");
-		listElem.innerHTML = "<input type=\"checkbox\" id=\""+trid(input.value.trim().split(' ').join('_')+"_"+questionID.id)+"\"/>";
-		listElem.innerHTML += "<label for=\""+trid(input.value.trim().split(' ').join('_')+"_"+questionID.id)+"\">"+htmlEntities(input.value.trim())+"</label>";
+		listElem.innerHTML = "<input type=\"checkbox\" id=\""+input.replace(/[^a-zA-Z]/g, "")+"_"+questionID.id+"\"/>";
+		listElem.innerHTML += "<label for=\""+input.replace(/[^a-zA-Z]/g, "")+"_"+questionID.id+"\">"+htmlEntities(input.trim())+"</label>";
 	console.log("1 OK");
-		input.value = "";
+		input = "";
 		document.getElementById(trid("list"+questionID.id)).appendChild(listElem);
 	console.log("1 OK");
 	}
